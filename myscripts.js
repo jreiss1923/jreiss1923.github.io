acd425c6ebd3478d9394d82ce433fc16102d70b7
jreiss1923 <joshua.reiss2@gmail.com>
2019-07-24 23:10:51 -0400

    Added javascript and colors to lines

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+function changeText() {
+    document.getElementById("intern-text").innerHTML='To learn more about what I did, click the link to the left'
+    document.getElementById("date-text-1").innerHTML='<p></p>'
+}
+
+function changeTextBack() {
+    document.getElementById("intern-text").innerHTML='<h3><span id="intern-text" class="small-text">Software Engineering Intern</span></h3>'
+    document.getElementById("date-text-1").innerHTML='<p><span id="date-text-1">October 2018 - August 2019</span></p>'
+}
