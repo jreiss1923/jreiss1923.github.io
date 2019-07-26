066d18ec2267e8882d4806faa9134abb2fb730b4
jreiss1923 <joshua.reiss2@gmail.com>
2019-07-25 21:39:09 -0400

    Added background color and vertical lines
    Todo:
    Change tab color
    Make background go all the way down the page
    Change onhover to slightly darker background
    Add more social media links

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,35 @@
-function changeText() {
-    document.getElementById("intern-text").innerHTML='To learn more about what I did, click the link to the left'
-    document.getElementById("date-text-1").innerHTML='<p></p>'
+function changeTextIntern() {
+    document.getElementById("intern-text").innerHTML='To learn more about what I did, click the link to the left';
+    document.getElementById("date-text-1").innerHTML='<p></p>';
 }
 
-function changeTextBack() {
-    document.getElementById("intern-text").innerHTML='<h3><span id="intern-text" class="small-text">Software Engineering Intern</span></h3>'
-    document.getElementById("date-text-1").innerHTML='<p><span id="date-text-1">October 2018 - August 2019</span></p>'
+function changeTextBackIntern() {
+    document.getElementById("intern-text").innerHTML='<h3><span id="intern-text" class="small-text">Software Engineering Intern</span></h3>';
+    document.getElementById("date-text-1").innerHTML='<p><span id="date-text-1">October 2018 - August 2019</span></p>';
+}
+
+function changeTextTyper(){
+    document.getElementById("typer-text").innerHTML='To learn more about what I did, click the link to the left';
+    document.getElementById("date-text-3").innerHTML='<p></p>'
+}
+
+function changeTextBackTyper(){
+    document.getElementById("typer-text").innerHTML='<h3><span class="small-text" id="typer-text">Typing Speed Test</span></h3>';
+    document.getElementById("date-text-3").innerHTML='<p><span id="date-text-3">December 2018</span></p>';
+}
+
+function changeTextBoggle(){
+    document.getElementById("boggle-text").innerHTML='To learn more about what I did, click the link to the right';
+    document.getElementById("date-text-2").innerHTML='<p></p>'
+}
+
+function changeTextBackBoggle(){
+    document.getElementById("boggle-text").innerHTML='<h3><span id="boggle-text" class="small-text">Boggle Solver</span></h3>'
+    document.getElementById("date-text-2").innerHTML='<p><span id="date-text-2">July 2019</span></p>'
+}
+
+function getBorderColor(){
+    var color = document.body.style.backgroundColor;
+    document.body.style.backgroundColor = "white";
+    
 }
