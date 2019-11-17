55a262f0bacd634d6f11062a19291ab79a0b7be7
jreiss1923 <joshua.reiss2@gmail.com>
2019-11-17 11:53:32 -0500

    added website link

--- myscripts.js
+++ myscripts.js
@@ -8,16 +8,6 @@ function changeTextBackIntern() {
     document.getElementById("date-text-1").innerHTML='<p><span id="date-text-1">October 2018 - August 2019</span></p>';
 }
 
-function changeTextTyper(){
-    document.getElementById("typer-text").innerHTML='To learn more about what I did, click the link to the left';
-    document.getElementById("date-text-3").innerHTML='<p></p>'
-}
-
-function changeTextBackTyper(){
-    document.getElementById("typer-text").innerHTML='<h3><span class="small-text" id="typer-text">Typing Speed Test</span></h3>';
-    document.getElementById("date-text-3").innerHTML='<p><span id="date-text-3">December 2018</span></p>';
-}
-
 function changeTextBoggle(){
     document.getElementById("boggle-text").innerHTML='To learn more about what I did, click the link to the right';
     document.getElementById("date-text-2").innerHTML='<p></p>'
@@ -26,4 +16,24 @@ function changeTextBoggle(){
 function changeTextBackBoggle(){
     document.getElementById("boggle-text").innerHTML='<h3><span id="boggle-text" class="small-text">Boggle Solver</span></h3>'
     document.getElementById("date-text-2").innerHTML='<p><span id="date-text-2">July 2019</span></p>'
+}
+
+function changeTextWebsite(){
+    document.getElementById("website-text").innerHTML='To learn more about what I did, click the link to the left';
+    document.getElementById("date-text-4").innerHTML='<p></p>'
+}
+
+function changeTextBackWebsite(){
+    document.getElementById("website-text").innerHTML='<h3><span class="small-text" id="website-text">Personal Website</span></h3>';
+    document.getElementById("date-text-4").innerHTML='<p><span id="date-text-4">August 2019</span></p>';
+}
+
+function changeTextTyper(){
+    document.getElementById("typer-text").innerHTML='To learn more about what I did, click the link to the left';
+    document.getElementById("date-text-3").innerHTML='<p></p>'
+}
+
+function changeTextBackTyper(){
+    document.getElementById("typer-text").innerHTML='<h3><span class="small-text" id="typer-text">Typing Speed Test</span></h3>';
+    document.getElementById("date-text-3").innerHTML='<p><span id="date-text-3">December 2018</span></p>';
 }
