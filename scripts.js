c75366d371a7e578d2616a41de99146becc7af73
jreiss1923 <joshua.reiss2@gmail.com>
2024-07-07 20:49:24 -0400

    add about tab

--- scripts.js
+++ scripts.js
@@ -19,6 +19,14 @@ function showJobs() {
         $('#linksbutton').addClass('unclicked');
         $('#linksbutton').removeClass('clicked');
     }
+    if ($('#about').hasClass('displayTrue')) {
+        $('#about').addClass('displayFalse');
+        $('#about').removeClass('displayTrue');
+    }
+    if ($('#aboutbutton').hasClass('clicked')) {
+        $('#aboutbutton').addClass('unclicked');
+        $('#aboutbutton').removeClass('clicked');
+    }
 }
 
 function showInterests() {
@@ -42,6 +50,14 @@ function showInterests() {
         $('#linksbutton').addClass('unclicked');
         $('#linksbutton').removeClass('clicked');
     }
+    if ($('#about').hasClass('displayTrue')) {
+        $('#about').addClass('displayFalse');
+        $('#about').removeClass('displayTrue');
+    }
+    if ($('#aboutbutton').hasClass('clicked')) {
+        $('#aboutbutton').addClass('unclicked');
+        $('#aboutbutton').removeClass('clicked');
+    }
 }
 
 function showLinks() {
@@ -64,5 +80,44 @@ function showLinks() {
     if ($('#interestsbutton').hasClass('clicked')) {
         $('#interestsbutton').addClass('unclicked');
         $('#interestsbutton').removeClass('clicked');
+    }
+    if ($('#about').hasClass('displayTrue')) {
+        $('#about').addClass('displayFalse');
+        $('#about').removeClass('displayTrue');
+    }
+    if ($('#aboutbutton').hasClass('clicked')) {
+        $('#aboutbutton').addClass('unclicked');
+        $('#aboutbutton').removeClass('clicked');
+    }
+}
+
+function showAbout() {
+    $('#about').addClass('displayTrue');
+    $('#about').removeClass('displayFalse');
+    $('#aboutbutton').addClass('clicked');
+    $('#aboutbutton').removeClass('unclicked');
+    if ($('#interests').hasClass('displayTrue')) {
+        $('#interests').addClass('displayFalse');
+        $('#interests').removeClass('displayTrue');
+    }
+    if ($('#jobs').hasClass('displayTrue')) {
+        $('#jobs').addClass('displayFalse');
+        $('#jobs').removeClass('displayTrue');
+    }
+    if ($('#jobsbutton').hasClass('clicked')) {
+        $('#jobsbutton').addClass('unclicked');
+        $('#jobsbutton').removeClass('clicked');
+    }
+    if ($('#interestsbutton').hasClass('clicked')) {
+        $('#interestsbutton').addClass('unclicked');
+        $('#interestsbutton').removeClass('clicked');
+    }
+    if ($('#links').hasClass('displayTrue')) {
+        $('#links').addClass('displayFalse');
+        $('#links').removeClass('displayTrue');
+    }
+    if ($('#linksbutton').hasClass('clicked')) {
+        $('#linksbutton').addClass('unclicked');
+        $('#linksbutton').removeClass('clicked');
     }
 }
